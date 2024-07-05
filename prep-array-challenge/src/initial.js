83ff5cdb1d2718f587b44310a6ee0a664866fbfd
Tristan Chao <chaot372@gmail.com>
2024-07-05 17:14:32 +0000

    Complete prep-array-challenge exercise.

---
+++ prep-array-challenge/src/initial.js
@@ -0,0 +1,10 @@
+/* exported initial */
+function initial(array) {
+  let initialArray = [];
+
+  for (let i = 0; i < array.length - 1; i++) {
+    initialArray.push(array[i]);
+  }
+
+  return initialArray;
+}
